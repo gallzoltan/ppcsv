40029c04da000c1e96bc765d2514c8a1e15153c1
Gáll Zoltán <gallzoltan@gmail.com>
2020-11-08 17:53:27 +0100

    create failed and ready lists

--- excel/load.go
+++ excel/load.go
@@ -2,6 +2,8 @@ package excel
 
 import (
 	"log"
+	"regexp"
+	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
@@ -11,6 +13,7 @@ type Excelrow struct {
 	Pcode      string
 	City       string
 	Address    string
+	DefAdr     string
 	Email      string
 	Tel        string
 	Device     string
@@ -35,9 +38,13 @@ func (d Excelrow) ReadXLSX(f string) {
 
 	for rows.Next() {
 		row := rows.Columns()
+		adr := splitAddress(row[1])
 		rawmap = append(rawmap, Excelrow{
 			Name:       row[0],
-			Address:    row[1],
+			Pcode:      adr[0],
+			City:       adr[1],
+			Address:    adr[2],
+			DefAdr:     row[1],
 			Email:      row[2],
 			Tel:        row[3],
 			Device:     row[4],
@@ -58,14 +65,34 @@ func (d Excelrow) GetFailedMap() []Excelrow {
 	return failedmap
 }
 
-func splitAddress(adr string) []string {
-	var result []string
+func splitAddress(line string) [3]string {
+	//\b(\d{4})(?!(\/|\)))\b
+	//\b[1-9]\d{3}\b(?!(\/|\)))
+	var valid = regexp.MustCompile(`\b^[1-9]\d{3}\b`)
+	result := [3]string{"", "", ""}
+
+	if valid.MatchString(line) {
+		result[0] = valid.FindString(line)
+		line = valid.ReplaceAllString(line, "")
+	}
+
+	if strings.Contains(line, ",") {
+		adr := strings.Split(line, ",")
+		result[1] = adr[0]
+		result[2] = adr[1]
+	} else {
+		result[2] = line
+	}
 	return result
 }
 
 func validateDatas() {
-	readymap = rawmap
-	// for _, row := range rawmap {
-	// 	log.Println(row.Address)
-	// }
+	//readymap = rawmap
+	for _, row := range rawmap {
+		if row.City == "" {
+			failedmap = append(failedmap, row)
+		} else {
+			readymap = append(readymap, row)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,19 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/gallzoltan/ppcsv/excel"
 )
+
+func saveCsv(datas []excel.Excelrow, fn string) {
+	f, err := os.Create(fn)
+	if err != nil {
+		log.Fatal(err)
+		f.Close()
+		return
+	}
+	for _, row := range datas {
+		fmt.Fprintf(f, "%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\r\n",
+			row.Name,
+			row.Pcode,
+			row.City,
+			row.Address,
+			row.DefAdr,
+			row.Email,
+			row.Tel,
+			row.Device,
+			row.Sn,
+			row.Srvname,
+			row.Buyingdate,
+			row.Startdate)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
+	err = f.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	log.Printf("file %s written successfully", fn)
+}
 
 func main() {
 	log.Println("The program has started...")
 
 	d := new(excel.Excelrow)
 	d.ReadXLSX("./bin/testfiles/568588.xlsx")
-	for _, row := range d.GetReadyMap() {
-		log.Printf("%s;%s;%s", row.Address, row.Name, row.Buyingdate)
-	}
+	saveCsv(d.GetReadyMap(), "readylist.csv")
+	saveCsv(d.GetFailedMap(), "failedlist.csv")
 
 	log.Println("The program has been finished.")
 }
